5b9b463cff283ce12127ffbab37db924f18c9775
dhikabotak99 <wicakbotak99@gmail.com>
2020-12-16 21:23:32 +0700

    Create carousel, card product

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,9 +15,11 @@
         </script> -->
     </head>
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
         <!-- NAVBAR -->
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="/">BOS Tani</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -28,13 +30,17 @@
                             <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Product</a>
+                            <a class="nav-link" href="#">Produk</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Investasi</a>
                         </li>
                     </ul>
                 </div>
                 <form class="d-flex">
                     <button class="btn btn-outline-success" type="submit">Login</button>
                 </form>
+                </div>
             </nav>
         </header>
         <!-- END OF NAVBAR -->
@@ -52,22 +58,24 @@
             </li>
         </ul> -->
         <!-- END OF SIDE BAR -->
-
-        <!-- <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <br>
+        <br>
+        <br>
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"></li>
                 <li data-bs-target="#carouselExampleDark" data-bs-slide-to="1"></li>
                 <li data-bs-target="#carouselExampleDark" data-bs-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2019/12/09/15731582.jpg" class="d-block w-100">
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="https://foto.kontan.co.id/zWePR7yKajLWaEMfJgAIOwAA9Ag=/smart/2014/09/08/31516295p.jpg" class="d-block w-100">
+                <div class="carousel-item active">
+                    <img src="/images/tomat.jpg" class="d-block" height="465px" width="1349px">
                 </div>
                 <div class="carousel-item">
-                <img src="https://ecs7.tokopedia.net/img/cache/700/VqbcmM/2020/5/5/97b96e97-2886-443a-855d-2e02715f5197.jpg" class="d-block w-100">
+                    <img src="/images/brokoli.jpg" class="d-block" height="465px" width="1349px">
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/bayam.jpg" class="d-block" height="465px" width="1349px">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleDark" role="button" data-bs-slide="prev">
@@ -78,28 +86,27 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </a>
-        </div> -->
-
-        <br>
-            @if(count($product)>0)
-            <div class="row g-3 d-flex justify-content-center">
-                @foreach($product as $p)
-                <div class="card" style="width: 18rem;">
-                    <img src="{{url('/images/'.$p->img_path)}}" class="card-img-top" height="262px" width="262px">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$p->Nama}}</h5>
-                        <p class="card-text">{{$p->Deskripsi}}</p>
-                        <h3 class="card-title">${{$p->Harga}}.00</h3>
-                        <a href="{{url('/order-product/'.$p->id)}}" class="btn btn-success">Order Now</a>
-                    </div>
-                </div>
-                &nbsp;&nbsp;
-                @endforeach
-            </div>
-            @else
-            <p>There is no data ...</p>
-            <a href="{{url('/input-product')}}"><button class="btn btn-dark">Add Product</button></a>
-            @endif
         </div>
+        <br>
+        @if(count($product)>0)
+        <div class="row g-3 d-flex justify-content-center">
+            @foreach($product as $p)
+            <div class="card" style="width: 18rem;">
+                <img src="{{url('/images/'.$p->img_path)}}" class="card-img-top" height="262px" width="262px">
+                <div class="card-body">
+                    <h5 class="card-title">{{$p->Nama}}</h5>
+                    <p class="card-text">{{$p->Deskripsi}}</p>
+                    <h3 class="card-title">${{$p->Harga}}.00</h3>
+                    <a href="{{url('/order-product/'.$p->id)}}" class="btn btn-success">Order Now</a>
+                </div>
+            </div>
+            &nbsp;&nbsp;
+            @endforeach
+        </div>
+        @else
+        <center>
+            <p>There is no product...</p>
+        </center>
+        @endif
     </body>
 </html>
